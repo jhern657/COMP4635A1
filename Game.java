8f173f77cb6b95f96a24e636164535e5ee307565
jhern657 <47986936+jhern657@users.noreply.github.com>
2022-02-15 16:20:50 -0700

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,20 +1,20 @@
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Random;
 
 public class Game {
-	
+
 	int level;
 	int failed_attempts;
+	int f_a_counter;
 	String line;
 	RandomAccessFile reader;
 	String phrase;
-	
+
+
 	static final int NUM_OF_WORDS = 240000;
-	
+
 	public Game(int input_level, int input_failed_attempts) {
 		level = input_level;
 		failed_attempts = input_failed_attempts;
@@ -23,34 +23,35 @@ public class Game {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 		phrase = assemble_phrase(level, reader);
-		
+		hide_phrase(phrase);
+
+		System.out.println("phrase = " + phrase);
 
 	}
 
-	
 	//asks for i(level) random words from the word repository,
 	// and concatenates them to create the phrase for a game round
 	public static String assemble_phrase(int level, RandomAccessFile reader) {
-		
+
 		Random random = new Random();
 		int randomNum = random.nextInt(NUM_OF_WORDS);
 		StringBuilder phrase = new StringBuilder();
-		
+
 		for(int i = 0; i < level; i++) {
 			// Grab one word and append it to the phrase
 			try {
 				// Move to a random spot
 				randomNum = random.nextInt(NUM_OF_WORDS);
 				reader.seek(randomNum);
-				
+
 				// Read until end of word
 				reader.readLine();
-				
+
 				// Append word to phrase
 				phrase.append(reader.readLine());
-				
+
 				// Add space if needed
 				if(i < level - 1)
 					phrase.append(" ");
@@ -59,9 +60,38 @@ public class Game {
 				e.printStackTrace();
 			}
 		}
-		
-		
+
+
 		return phrase.append("\n").toString();
-		
+
 	}
+
+	// hide words behind '-' and show failed attempt counter
+	public void hide_phrase(String phrase) {
+
+	    char[] letters = phrase.toCharArray();
+
+	    for (int i=0; i < letters.length; i++) {
+
+	        switch(letters[i]) {
+	            case ' ': //check if current char is a space
+	                System.out.print(" ");
+	                i++;
+	            case '\n': //check if current char is a newline character
+	                i++;
+	            default:
+	                System.out.print("-"); //replace each character with -
+	        }
+
+	    }
+
+	    f_a_counter = level * failed_attempts;
+
+	    System.out.println("C"+f_a_counter); // set failed attempt counter
+
+	}
+
+
+
+
 }
